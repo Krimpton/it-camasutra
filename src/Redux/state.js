df8a7bf527804ee3c83f6c1d4fe1446f76dcbc7d
Krimpton <hesoyam.exe@gmail.com>
2020-07-19 01:11:11 +0300

    release v1.1ref

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,89 +1,95 @@
-import {rerenderEntireTree} from "../render";
+let store = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: 0, sms: 'Hi, hello', likeCount: 0},
+                {id: 1, sms: 'Wow, Git!', likeCount: 1},
+            ],
+            newPostText: ''
 
-let state = {
-    profilePage: {
-        postsData: [
-            {id: 0, sms: 'Hi, hello', likeCount: 0},
-            {id: 1, sms: 'Wow, Git!', likeCount: 1},
-        ],
-        newPostText: ''
+
+        },
+        messagesPage: {
+            messageData: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Alex'},
+                {id: 3, name: 'Andry'},
+                {id: 4, name: 'Michail'},
+                {id: 5, name: 'Vova'},
+                {id: 6, name: 'Vadim'},
+                {id: 7, name: 'Ignat'},
+            ],
+            messageInfo: [
+                {id: 1, sms: 'Hello'},
+                {id: 2, sms: 'play ow'},
+                {id: 3, sms: 'What do you need bro?'},
+                {id: 4, sms: 'play overwatch now'},
+                {id: 5, sms: 'How do you do?!'},
+                {id: 6, sms: 'Just do it!!'},
+                {id: 7, sms: 'come to mee again'},
+            ],
+            newMessageText: '666111'
+        },
 
 
+        sidebarPage: {
+            sidebar: [
+                {id: 1, name: 'Dimych', icon: 'icon'},
+                {id: 2, name: 'Alex', icon: 'icon'},
+                {id: 3, name: 'Andry', icon: 'icon'},
+                {id: 4, name: 'Michail', icon: 'icon'},
+                {id: 5, name: 'Vova', icon: 'icon'},
+                {id: 6, name: 'Vadim', icon: 'icon'},
+                {id: 7, name: 'Ignat', icon: 'icon'},
+            ],
+        }
     },
-    messagesPage: {
-        messageData: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Alex'},
-            {id: 3, name: 'Andry'},
-            {id: 4, name: 'Michail'},
-            {id: 5, name: 'Vova'},
-            {id: 6, name: 'Vadim'},
-            {id: 7, name: 'Ignat'},
-        ],
-        messageInfo: [
-            {id: 1, sms: 'Hello'},
-            {id: 2, sms: 'play ow'},
-            {id: 3, sms: 'What do you need bro?'},
-            {id: 4, sms: 'play overwatch now'},
-            {id: 5, sms: 'How do you do?!'},
-            {id: 6, sms: 'Just do it!!'},
-            {id: 7, sms: 'come to mee again'},
-        ],
-        newMessageText: '666111'
+    getState(){
+        return this._state;
     },
-
-
-    sidebarPage: {
-        sidebar: [
-            {id: 1, name: 'Dimych', icon: 'icon'},
-            {id: 2, name: 'Alex', icon: 'icon'},
-            {id: 3, name: 'Andry', icon: 'icon'},
-            {id: 4, name: 'Michail', icon: 'icon'},
-            {id: 5, name: 'Vova', icon: 'icon'},
-            {id: 6, name: 'Vadim', icon: 'icon'},
-            {id: 7, name: 'Ignat', icon: 'icon'},
-        ],
+    _callSubscriber(){
+        console.log('state has changed');
+    },
+    addPost() {
+        const newPost = {
+            id: 5,
+            sms: this._state.profilePage.newPostText,
+            likeCount: 0
+        };
+        // Пушит данные из textArea в конец масива из state
+        this._state.profilePage.postsData.push(newPost);
+        // Зануление textArea
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText){
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage() {
+        const newMes = {
+            id: 0,
+            sms: this._state.messagesPage.newMessageText
+        };
+        // Пушит данные из textArea в конец масива из state
+        this._state.messagesPage.messageInfo.push(newMes);
+        // Зануление textArea
+        this._state.messagesPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newMessage){
+        this._state.messagesPage.newMessageText = newMessage;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
+    // методы
 }
 
-window.state = state;
+
+window.store = store;
 // делает state глобальным для чтения
 
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        sms: state.profilePage.newPostText,
-        likeCount: 0
-    };
-    // Пушит данные из textArea в конец масива из state
-    state.profilePage.postsData.push(newPost);
-    // Зануление textArea
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-
-export let addMessage = () => {
-    let newMes = {
-        id: 0,
-        sms: state.messagesPage.newMessageText
-    };
-    // Пушит данные из textArea в конец масива из state
-    state.messagesPage.messageInfo.push(newMes);
-    // Зануление textArea
-    // state.messagesPage.newMessageText = '';
-    rerenderEntireTree(state);  
-}
-
-export let updateNewMessageText = (newMessage) => {
-    state.messagesPage.newMessageText = newMessage;
-    rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from './render';
-import state from "./Redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import store from "./Redux/state";
 
-rerenderEntireTree(state);
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state}
+                 // бинд метода стор да бы сохранить владельца метода
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)}
+                 addMessage={store.addMessage.bind(store)}/>
+        </BrowserRouter>, document.getElementById('root'));
+}
+rerenderEntireTree(store.getState());
+
+
+store.subscribe(rerenderEntireTree);
+
+
 
 serviceWorker.unregister();
